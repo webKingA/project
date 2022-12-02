35ca0063064ef8e37a6eacb658ed0c2755ecf727
unknown <yazdan.jvd.81@gmail.com>
2022-12-02 17:34:14 +0330

    add profile admin part 2

--- Components/ProfileAdmin/Table/Table.jsx
+++ Components/ProfileAdmin/Table/Table.jsx
@@ -4,6 +4,9 @@ import style from "./Table.module.css";
 // import Mui Start
 
 import Pagination from "@mui/material/Pagination";
+import Switch from "@mui/material/Switch";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 
 // import Mui End
@@ -16,6 +19,7 @@ import { GrEdit } from "react-icons/gr";
 // import Icons End
 
 const Table = () => {
+  const label = { inputProps: { "aria-label": "Switch demo" } };
   return (
     <div className={style.table__container}>
       <div className={style.pagination__container}>
@@ -23,16 +27,247 @@ const Table = () => {
       </div>
       <div className={style.information__table}>
         <table>
-            <thead>
-                <th>
-                    <tr></tr>
-                    <tr></tr>
-                    <tr></tr>
-                    <tr></tr>
-                    <tr></tr>
-                </th>
-            </thead>
-            <tbody></tbody>
+          <thead className={style.thead}>
+            <th>
+              <tr>نام کاربر</tr>
+              <tr>نام کاربری</tr>
+              <tr>موبایل</tr>
+              <tr>وضعیت</tr>
+              <tr>عملیات</tr>
+            </th>
+          </thead>
+          <tbody className={style.tbody}>
+            <td>
+              <tr>null null</tr>
+              <tr>09120148604</tr>
+              <tr> 09120148604</tr>
+              <tr>
+                <Stack
+                  direction="row"
+                  spacing={1}
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <Typography>Off</Typography>
+                  <Switch {...label} defaultChecked />
+                  <Typography>On</Typography>
+                </Stack>
+              </tr>
+              <tr>
+                <Button
+                  variant="text"
+                  startIcon={<AiOutlinePlus />}
+                  style={{
+                    padding: "10px",
+                    fontFamily: "v",
+                    fontSize: "16px",
+                    display: "flex",
+                    gap: "1rem",
+                  }}
+                >
+                  انتصاب نقش
+                </Button>
+                <Button
+                  variant="contained"
+                  startIcon={<GrEdit />}
+                  style={{
+                    padding: "5px 15px",
+                    fontFamily: "v",
+                    fontSize: "16px",
+                    display: "flex",
+                    gap: "1rem",
+                    backgroundColor: "#654fef",
+                  }}
+                >
+                  ویرایش
+                </Button>
+              </tr>
+            </td>
+            <td>
+              <tr>null null</tr>
+              <tr>09120148604</tr>
+              <tr> 09120148604</tr>
+              <tr>
+                <Stack
+                  direction="row"
+                  spacing={1}
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <Typography>Off</Typography>
+                  <Switch {...label} defaultChecked />
+                  <Typography>On</Typography>
+                </Stack>
+              </tr>
+              <tr>
+                <Button
+                  variant="text"
+                  startIcon={<AiOutlinePlus />}
+                  style={{
+                    padding: "10px",
+                    fontFamily: "v",
+                    fontSize: "16px",
+                    display: "flex",
+                    gap: "1rem",
+                  }}
+                >
+                  انتصاب نقش
+                </Button>
+                <Button
+                  variant="contained"
+                  startIcon={<GrEdit />}
+                  style={{
+                    padding: "5px 15px",
+                    fontFamily: "v",
+                    fontSize: "16px",
+                    display: "flex",
+                    gap: "1rem",
+                    backgroundColor: "#654fef",
+                  }}
+                >
+                  ویرایش
+                </Button>
+              </tr>
+            </td>
+            <td>
+              <tr>null null</tr>
+              <tr>09120148604</tr>
+              <tr> 09120148604</tr>
+              <tr>
+                <Stack
+                  direction="row"
+                  spacing={1}
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <Typography>Off</Typography>
+                  <Switch {...label} defaultChecked />
+                  <Typography>On</Typography>
+                </Stack>
+              </tr>
+              <tr>
+                <Button
+                  variant="text"
+                  startIcon={<AiOutlinePlus />}
+                  style={{
+                    padding: "10px",
+                    fontFamily: "v",
+                    fontSize: "16px",
+                    display: "flex",
+                    gap: "1rem",
+                  }}
+                >
+                  انتصاب نقش
+                </Button>
+                <Button
+                  variant="contained"
+                  startIcon={<GrEdit />}
+                  style={{
+                    padding: "5px 15px",
+                    fontFamily: "v",
+                    fontSize: "16px",
+                    display: "flex",
+                    gap: "1rem",
+                    backgroundColor: "#654fef",
+                  }}
+                >
+                  ویرایش
+                </Button>
+              </tr>
+            </td>
+            <td>
+              <tr>null null</tr>
+              <tr>09120148604</tr>
+              <tr> 09120148604</tr>
+              <tr>
+                <Stack
+                  direction="row"
+                  spacing={1}
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <Typography>Off</Typography>
+                  <Switch {...label} defaultChecked />
+                  <Typography>On</Typography>
+                </Stack>
+              </tr>
+              <tr>
+                <Button
+                  variant="text"
+                  startIcon={<AiOutlinePlus />}
+                  style={{
+                    padding: "10px",
+                    fontFamily: "v",
+                    fontSize: "16px",
+                    display: "flex",
+                    gap: "1rem",
+                  }}
+                >
+                  انتصاب نقش
+                </Button>
+                <Button
+                  variant="contained"
+                  startIcon={<GrEdit />}
+                  style={{
+                    padding: "5px 15px",
+                    fontFamily: "v",
+                    fontSize: "16px",
+                    display: "flex",
+                    gap: "1rem",
+                    backgroundColor: "#654fef",
+                  }}
+                >
+                  ویرایش
+                </Button>
+              </tr>
+            </td>
+            <td>
+              <tr>null null</tr>
+              <tr>09120148604</tr>
+              <tr> 09120148604</tr>
+              <tr>
+                <Stack
+                  direction="row"
+                  spacing={1}
+                  alignItems="center"
+                  justifyContent="center"
+                >
+                  <Typography>Off</Typography>
+                  <Switch {...label} defaultChecked />
+                  <Typography>On</Typography>
+                </Stack>
+              </tr>
+              <tr>
+                <Button
+                  variant="text"
+                  startIcon={<AiOutlinePlus />}
+                  style={{
+                    padding: "10px",
+                    fontFamily: "v",
+                    fontSize: "16px",
+                    display: "flex",
+                    gap: "1rem",
+                  }}
+                >
+                  انتصاب نقش
+                </Button>
+                <Button
+                  variant="contained"
+                  startIcon={<GrEdit />}
+                  style={{
+                    padding: "5px 15px",
+                    fontFamily: "v",
+                    fontSize: "16px",
+                    display: "flex",
+                    gap: "1rem",
+                    backgroundColor: "#654fef",
+                  }}
+                >
+                  ویرایش
+                </Button>
+              </tr>
+            </td>
+          </tbody>
         </table>
       </div>
     </div>
